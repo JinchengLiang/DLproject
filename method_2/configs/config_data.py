2dd084370d2d979648881f0331fc99dacf5984f2
Shaomin Xu <xushaominchina@163.com>
2023-05-16 10:44:09 -0400

    Update config_data.py

--- method_2/configs/config_data.py
+++ method_2/configs/config_data.py
@@ -1,6 +1,6 @@
 
 DATA_CONFIG = {
-    'bar': 4,
+    'bar': 8,
     'ts_per_bar': 16,
     'feature_size': 800,    # (freq_up-freq_low+2)*ts_per_bar+ts_per_bar
     'freq_up': 95,
@@ -8,13 +8,10 @@ DATA_CONFIG = {
     'testset_ratio': 0.1,
     'data_tpb': 4,
     'tt_num': 3600,  # The tt samples num for training
-    'data_path': "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/",
-    'ch_data_path':
-        "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/ch/Musics/",
-    'tt_data_path':
-        "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/tt/",
-    'debug_data_path':
-        "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/debug/",
-    'ch_npy': "ch_m_0516.npy",
-    'tt_npy': "tt_m_0516.npy"
+    'data_path': "../data/",
+    'ch_data_path':"../dataset/ChMusicMIDI8bar/",
+    'tt_data_path':"../dataset/LakhMIDI/",
+    'debug_data_path': "../dataset/debug/",
+    'ch_npy': "ch_m.npy",
+    'tt_npy': "tt_m.npy"
 }
