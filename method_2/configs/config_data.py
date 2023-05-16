62a85566377dbfe613b26521d58371b686a9037a
Shaomin Xu <xushaominchina@163.com>
2023-05-16 10:04:38 -0400

    create method_2

---
+++ method_2/configs/config_data.py
@@ -0,0 +1,20 @@
+
+DATA_CONFIG = {
+    'bar': 4,
+    'ts_per_bar': 16,
+    'feature_size': 800,    # (freq_up-freq_low+2)*ts_per_bar+ts_per_bar
+    'freq_up': 95,
+    'freq_low': 48,
+    'testset_ratio': 0.1,
+    'data_tpb': 4,
+    'tt_num': 3600,  # The tt samples num for training
+    'data_path': "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/",
+    'ch_data_path':
+        "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/ch/Musics/",
+    'tt_data_path':
+        "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/tt/",
+    'debug_data_path':
+        "C:/Users/13516/Desktop/2023 Spring/ECE-GY 6953 Deep Learning/DLproject/data/debug/",
+    'ch_npy': "ch_m_0516.npy",
+    'tt_npy': "tt_m_0516.npy"
+}
